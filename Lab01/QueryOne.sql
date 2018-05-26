f28e0aa223dea697ab8cc2d927efcfb1b76026ae
jcastilla@vallegrande.edu.pe <jcastilla@vallegrande.edu.pe>
2018-05-26 10:17:33 -0500

    Estoy subiendo el listado de las tablas de mi base de datos

--- Lab01/QueryOne.sql
+++ Lab01/QueryOne.sql
@@ -6,3 +6,11 @@ GO
 SELECT DATABASE_ID, name
 FROM sys.databases;
 GO
+
+/* Poner en uso una base de datos*/
+USE HospitalHHH;
+go
+
+/*Ver listado de tablas de una base de datos*/
+USE AdventureWorks2016
+select name from sys.tables;
