0506be2aef153187ad4f1b2838b8f0c6854dcde0
jcastilla@vallegrande.edu.pe <jcastilla@vallegrande.edu.pe>
2018-05-26 10:01:49 -0500

    Listado de Base de Datos

--- Lab01/QueryOne.sql
+++ Lab01/QueryOne.sql
@@ -1,4 +1,8 @@
 /* Crear una BD en blanco*/
 create DATABASE HospitalHHH;
 GO
+
 /*Necesito visualizar el listado de base de datos de tu servidor local*/
+SELECT DATABASE_ID, name
+FROM sys.databases;
+GO
